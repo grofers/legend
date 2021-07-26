481e19c2565cf37c1756b7f85b9ae31855f4c370
Sumanth Reddy <sumanthkumarc@users.noreply.github.com>
2021-07-26 16:03:50 +0530

    1. Added fill expression to fill in null data points for cloud watch ELB, ALB Metrics. 2. Bumped up minikube version in Github actions

--- legend/__init__.py
+++ legend/__init__.py
@@ -16,7 +16,9 @@ GRAFONNET_REPO_URL = os.getenv(
     "https://github.com/grafana/grafonnet-lib"
 )
 GRAFONNET_REPO_NAME = os.getenv("GRAFONNET_REPO_NAME", "grafonnet-lib")
-GRAFONNET_REPO_RELEASE_TAG = os.getenv("GRAFONNET_REPO_RELEASE_TAG", "v0.1.0")
+# Switching release tag to master because there has been no new release for a very long time and we needed fixes from newer commits.
+# https://github.com/grafana/grafonnet-lib/commit/82ccf9013f14d976b83a01a43f5f062019cbee2c
+GRAFONNET_REPO_RELEASE_TAG = os.getenv("GRAFONNET_REPO_RELEASE_TAG", "master")
 
 GRAFANA_DEFAULT_DATA_SOURCES = {
     "PROMETHEUS": os.environ.get("GRAFANA_DEFAULT_PROMETHEUS_DATA_SOURCE"),
